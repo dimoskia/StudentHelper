3c2d2dc5cff46a67563fed2bfc89a0c3bb4b7fc1
Aleksandar <dimoskialeksandar11@gmail.com>
2020-01-22 18:23:03 +0100

    Added JWT authorization

---
+++ StudentHelper/Auth/JwtAuthentication.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace JwtExample.Auth
+{
+    public class JwtAuthentication : Attribute, IAuthenticationFilter
+    {
+        public string Realm { get; set; }
+        public bool AllowMultiple => false;
+
+        public string AllowedRole;
+
+        public JwtAuthentication(string AllowedRole = "user")
+        {
+            this.AllowedRole = AllowedRole;
+        }
+
+        public async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            var request = context.Request;
+
+            var authorization = request.Headers.Authorization;
+            // checking request header value having required scheme "Bearer" or not.
+            if (authorization == null || authorization.Scheme != "Bearer" || string.IsNullOrEmpty(authorization.Parameter))
+            {
+                context.ErrorResult = new AuthFailureResult("JWT Token is Missing", request);
+                return;
+            }
+            // Getting Token value from header values.
+            var token = authorization.Parameter;
+            var principal = await AuthJwtToken(token);
+
+            if (principal == null)
+            {
+                context.ErrorResult = new AuthFailureResult("Invalid JWT Token", request);
+            }
+            else
+            {
+                context.Principal = principal;
+            }
+        }
+        
+        protected Task<IPrincipal> AuthJwtToken(string token)
+        {
+            string email;
+            string role;
+            
+
+            if (ValidateToken(token, out email, out role))
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim(ClaimTypes.Role, role)
+                };
+
+                var identity = new ClaimsIdentity(claims, "Jwt");
+                IPrincipal user = new ClaimsPrincipal(identity);
+
+                return Task.FromResult(user);
+            }
+
+            return Task.FromResult<IPrincipal>(null);
+        }
+
+        private bool ValidateToken(string token, out string email, out string role)
+        {
+            email = null;
+            role = null;
+
+            var simplePrinciple = JwtAuthManager.GetPrincipal(token);
+            if (simplePrinciple == null)
+                return false;
+            var identity = simplePrinciple.Identity as ClaimsIdentity;
+
+            if (identity == null)
+                return false;
+
+            if (!identity.IsAuthenticated)
+                return false;
+
+            var emailClaim = identity.FindFirst(ClaimTypes.Email);
+            email = emailClaim?.Value;
+
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            role = identity.FindFirst(ClaimTypes.Role)?.Value;
+            if (string.IsNullOrEmpty(role))
+                return false;
+
+            return role.Equals("admin") || role.Equals(AllowedRole);
+
+        }
+
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            Challenge(context);
+            return Task.FromResult(0);
+        }
+
+        private void Challenge(HttpAuthenticationChallengeContext context)
+        {
+            string parameter = null;
+
+            if (!string.IsNullOrEmpty(Realm))
+                parameter = "realm=\"" + Realm + "\"";
+
+            context.ChallengeWith("Bearer", parameter);
+        }
+    }
+}
